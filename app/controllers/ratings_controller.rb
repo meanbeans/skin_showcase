a3f812634e68865921a1294cfe94b61248ac753c
Jason Kim <iamjsonkim@gmail.com>
2016-09-17 19:45:52 -0700

    update existing rating for  a given skin

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,6 +2,9 @@ class RatingsController < ApplicationController
   before_action :require_login
 
   def create
+    existing_rating = Rating.find_by({ skin_id: ratings_params[:skin_id] })
+    return update_existing(existing_rating) if existing_rating
+
     @rating = Rating.new(ratings_params)
     @rating.user_id = current_user.id
 
@@ -22,5 +25,13 @@ class RatingsController < ApplicationController
 
     def require_login
       filter_require_login
+    end
+
+    def update_existing(existing_rating)
+      if existing_rating.update_attributes(ratings_params)
+        render json: @rating
+      else
+        flash.now[:error] = 'Failed to rate. Please try again.'
+      end
     end
 end
